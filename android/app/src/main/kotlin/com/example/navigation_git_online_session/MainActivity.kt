5ee333d13640961a6996e109f6d9a46ec159d6f8
Lenovo <samy.elmenshawy96@gmail.com>
2025-06-16 21:34:56 +0300

    change title color added to material app

---
+++ android/app/src/main/kotlin/com/example/navigation_git_online_session/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.navigation_git_online_session
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/first_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  static const String route = 'first screen';
+
+  FirstScreen({super.key});
+
+  String name = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(title: Text('login screen')));
+  }
+}
